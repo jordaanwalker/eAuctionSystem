0a608363a42210e34e65d60e83cc4b837784e700
jordanwalker <jordanwalker@192.168.0.3>
2019-03-20 20:05:06 +0000

    Removed comment on Buyer class.

--- eAuction System/src/Buyer.java
+++ eAuction System/src/Buyer.java
@@ -3,7 +3,6 @@ public class Buyer extends User{
 
 	public Buyer(String username, String password) {
 		super(username, password);
-		// TODO Auto-generated constructor stub
 	}
 
 	public void victory() {
